d69a13c5063073aa89d8cbe4794148a653e46839
Kritesh Timsina <kriteshtimsina55@gmail.com>
2023-09-03 15:23:34 +0545

    fix:song list styling

--- frontend/src/pages/Home/components/SongList.tsx
+++ frontend/src/pages/Home/components/SongList.tsx
@@ -2,20 +2,16 @@ import { ISong } from "./songs";
 
 const SongList = ({ song }: { song: ISong }) => {
   return (
-    <div className=" flex gap-10 md:gap-32 py-3 items-center w-full md:w-3/4  justify-start border-b-secondary border-b-[1px] pb-1 hover:bg-secondary cursor-pointer ">
-      <p className="text-2xl">{song.id}</p>
-      <div className="hidden md:flex items-center gap-3 ">
-        <img src={song.image} alt={song.track} className="w-10" />
-        <p className="text-2xl">{song.track}</p>
-        <span className="text-slate-400 text-sm">LYRICS</span>
-      </div>
-      <p className="text-2xl hidden md:block">{song.artist}</p>
-
-      <div className="flex items-center gap-3 md:hidden">
+    <div className="flex gap-10 md:gap-32 py-2 items-center w-full md:w-3/4  justify-start border-b-secondary border-b-[1px] pb-1 hover:bg-secondary cursor-pointer ">
+      <p className="text-7xl font-roboto font-bold text-stone-700 flex items-end gap-1">
+        {song.id}
+        <span className="h-3 w-3 bg-blue-500 mb-2"></span>
+      </p>
+      <div className="flex items-center gap-3 w-full">
         <img src={song.image} alt={song.track} className="w-20" />
-        <div className="flex-col gap-2">
-          <p className="text-2xl">{song.track}</p>
-          <p className="text-xl">{song.artist}</p>
+        <div className=" w-full flex flex-col gap-0 sm:gap-2 sm:flex-row sm:items-center ">
+          <p className="text-2xl w-fit whitespace-nowrap">{song.track}</p>
+          <p className="text-xl order-4 sm:text-end w-full ">{song.artist}</p>
           <span className="text-slate-400 text-sm">LYRICS</span>
         </div>
       </div>
