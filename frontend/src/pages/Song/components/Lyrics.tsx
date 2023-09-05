fd0a51b64eddf81333bd5812018de99584d52789
Kritesh Timsina <kriteshtimsina55@gmail.com>
2023-09-05 14:12:08 +0545

    deploy page

--- frontend/src/pages/Song/components/Lyrics.tsx
+++ frontend/src/pages/Song/components/Lyrics.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { AiOutlineFieldTime } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
 import { GiMicrophone } from "react-icons/gi";
