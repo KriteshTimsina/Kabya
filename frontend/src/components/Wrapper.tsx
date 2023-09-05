df1e1ebff7451adb8eb526eb7928add8cbb98d4d
Kritesh Timsina <kriteshtimsina55@gmail.com>
2023-09-05 14:25:36 +0545

    Updates

--- frontend/src/components/Wrapper.tsx
+++
@@ -1,9 +0,0 @@
-import { IWrapper } from "../utils/types";
-
-export const Wrapper = (props: IWrapper) => {
-  return (
-    <div className={`h-[calc(100%-80px)] pt-top ${props.className}`}>
-      {props.children}
-    </div>
-  );
-};
--- frontend/src/utils/types.ts
+++
@@ -1,4 +0,0 @@
-export interface IWrapper {
-    children: React.ReactNode;
-    className?: string;
-  }
