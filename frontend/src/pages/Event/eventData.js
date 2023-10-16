25de6f5a360d7a37f74a9e1cd07500fe05e40a04
Dilini Liyanage <ldsliyanage99@gmail.com>
2023-10-16 11:21:00 +0530

    [FRONTEND] Design events page #5
    #5

---
+++ frontend/src/pages/Event/eventData.js
@@ -0,0 +1,115 @@
+import selenaEvent from "../../assets/selena-event.jpg";
+import JustinEvent from "../../assets/justine-b.jpg"
+import Event3 from "../../assets/event3.jpg"
+export const eventData = [
+    {
+      eventName: "Concert in the Park",
+      description: "A musical evening to remember...",
+      date: "October 15, 2023",
+      time: "7:00 PM",
+      location: "Central Park, New York",
+      image: selenaEvent,
+      isFree: true,
+      isOver: false,
+    },
+    
+    {
+      eventName: "JB Live In Concert",
+      description: "An interactive physical concert...",
+      date: "October 15, 2023",
+      time: "7:00 PM",
+      location: "Central Park, New York",
+      image: JustinEvent,
+      price: "$ 20",
+      isFree: false,
+      isOver: true,
+    },
+
+    {
+      eventName: "Concert in the Park",
+      description: "A musical evening to remember...",
+      date: "October 15, 2023",
+      time: "7:00 PM",
+      location: "Central Park, New York",
+      image: selenaEvent,
+      isFree: true,
+      isOver: false,
+    },
+
+    {
+      eventName: "Concert in the Park",
+      description: "A musical evening to remember...",
+      date: "October 15, 2023",
+      time: "7:00 PM",
+      location: "Central Park, New York",
+      image: selenaEvent,
+      isFree: true,
+      isOver: false,
+    },
+
+    {
+      eventName: "Concert in the Park",
+      description: "A musical evening to remember...",
+      date: "October 15, 2023",
+      time: "7:00 PM",
+      location: "Central Park, New York",
+      image: selenaEvent,
+      isFree: true,
+      isOver: false,
+    },
+    
+    {
+      eventName: "Concert in the Park",
+      description: "A musical evening to remember...",
+      date: "October 15, 2023",
+      time: "7:00 PM",
+      location: "Central Park, New York",
+      image: selenaEvent,
+      isFree: true,
+      isOver: false,
+    },
+
+    {
+      eventName: "Concert in the Park",
+      description: "A musical evening to remember...",
+      date: "October 15, 2023",
+      time: "7:00 PM",
+      location: "Central Park, New York",
+      image: selenaEvent,
+      isFree: true,
+      isOver: false,
+    },
+
+    {
+      eventName: "Concert in the Park",
+      description: "A musical evening to remember...",
+      date: "October 15, 2023",
+      time: "7:00 PM",
+      location: "Central Park, New York",
+      image: selenaEvent,
+      isFree: true,
+      isOver: false,
+    },
+    {
+      eventName: "International Live Music",
+      description: "A musical evening to remember...",
+      date: "October 28, 2023",
+      time: "7:00 PM",
+      location: "28th street, New York",
+      image: Event3,
+      isFree: true,
+      isOver: false,
+    },
+    {
+      eventName: "Concert in the Park",
+      description: "A musical evening to remember...",
+      date: "October 15, 2023",
+      time: "7:00 PM",
+      location: "Central Park, New York",
+      image: selenaEvent,
+      isFree: true,
+      isOver: false,
+    },
+    
+  ];
+  
